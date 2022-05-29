73ac8cdb304c113c3c5ea68e1a720fe9a03f963e
Sanket Shinde <70633400+ssankey@users.noreply.github.com>
2022-05-29 20:28:21 +0530

    preloader

--- script.js
+++ script.js
@@ -10,13 +10,6 @@ closeBtn.onclick = function(){
     navLinks.style.right = "-200px";
 }
 
-const logo = document.querySelectorAll("#logo path");
-console.log(logo);
-for(i=0;i<=logo.length;i++){
-    console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
-}
-
-
 var loader = document.getElementById("preloader");
 window.addEventListener("load",function(){
     loader.style.display = "none";
